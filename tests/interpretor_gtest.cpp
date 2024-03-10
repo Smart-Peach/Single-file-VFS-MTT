87d71702ddc119a6c877575f88bbdd5f32fb0ee8
MahasApu <maryvask0@yandex.ru>
2024-03-10 20:08:07 +0700

    add tests for dir cmds

--- tests/interpretor_gtest.cpp
+++ tests/interpretor_gtest.cpp
@@ -9,9 +9,19 @@
 typedef std::vector<std::string> str_args_t;
 
 
-TEST(InterpretorGTest, writeCommand){
+TEST(InterpretorGTest, writeFileCommand){
     str_args_t all_args{"write", "test_file.py"};
     std::string excpected = "file test_file.py written";
+    std::cout << "Excpected:\n" << excpected << std::endl;
+    std::cout << "Actual:" << std::endl;
+    Interpretor* interpretor = new Interpretor(all_args);
+    interpretor->interpret();
+    delete interpretor;
+}
+
+TEST(InterpretorGTest, UnlinkDirCommand){
+    str_args_t all_args{"uldir", "nice-dir"};
+    std::string excpected = "directory nice-dir unlinked";
     std::cout << "Excpected:\n" << excpected << std::endl;
     std::cout << "Actual:" << std::endl;
     Interpretor* interpretor = new Interpretor(all_args);
--- tests/interpretor_test.cpp
+++ tests/interpretor_test.cpp
@@ -14,6 +14,11 @@ void CommandTest(size_t test_number, str_args_t all_args, std::string excpected)
 
 
 int main(int argc, char* argv[]) {
+    std::cout << "\n=================Start test running=================" << std::endl;
     CommandTest(1, str_args_t {"write", "ajjajaj.a"}, "file ajjajaj.a written");
-    CommandTest(2, str_args_t {"write"}, "No input file for command 'write'");
+    CommandTest(2, str_args_t {"write"}, "No src input for command 'write'");
+    CommandTest(3, str_args_t {"uldir"}, "No src input for command 'uldir'");
+    CommandTest(4, str_args_t {"odir", "nicedir"}, "Unknown command 'odir'");
+    CommandTest(5, str_args_t {"rndir", "nicedir"}, "directory nicedir renamed");
+    std::cout << "=================End test running=================" << std::endl;
 }
