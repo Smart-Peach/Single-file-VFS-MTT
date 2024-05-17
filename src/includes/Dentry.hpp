949701ab2c1dcec4f131f5bd5c187a11a6e84934
MahasApu <maryvask0@yandex.ru>
2024-05-17 16:13:44 +0700

    Dentry structure Fixes #8

--- src/includes/Dentry.hpp
+++ src/includes/Dentry.hpp
@@ -7,20 +7,18 @@
 
 
 class Dentry {
-    size_t  d_inode_index; // Associated with directory inode
-    Dentry* d_parent;      // Dentry object of parent
-    str_t   d_name;        // Name of directory
-    size_t  d_name_len;    // Length of directory's name (idk for what it's needed but let it be)
-    size_t  d_records_len; // length of directory's records (len of file) 
-    vector_size_t f_indexes_list; // List of files magic numbers
-public:
-    Dentry(size_t d_inode_index, str_t d_name, size_t d_name_len, Dentry* d_parent = nullptr):
-                                                            d_inode_index(d_inode_index),
-                                                            d_parent(d_parent),
-                                                            d_name(d_name),
-                                                            d_name_len(d_name_len),
-                                                            d_records_len(0) { }
+    Inode*          d_inode_index;  // Associated with directory inode
+    str_t           d_name;         // Name of directory
+    Dentry*         d_parent;       // Dentry object of parent
+    size_t          d_records_len;  // length of directory's records (len of dir file) 
+    vector_size_t   f_list;         // List of magic numbers of files 
+    vector_size_t   d_list;         // List of magic numbers of subdirectories
 
+    // may be it's better to have map of magic numbers 
+public:
+    Dentry(str_t d_name, Dentry* d_parent = nullptr): d_parent(d_parent),
+                                                      d_name(d_name),
+                                                      d_records_len(0) { }
     ~Dentry() { delete d_parent; }
 
     // Allocates the root dentry. 
@@ -36,8 +34,6 @@ public:
     // The lighter version of the previous call, in which the dentry 
     // was previously added in the hash table.
     bool d_create_dentry();
-
-
 
 
 };
