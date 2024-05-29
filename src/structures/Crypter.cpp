e3e0bdc738409d4cc0ac8bc6b830023b7aa123c1
PetrunevaTasia <hext2004t@gmail.com>
2024-05-29 12:02:19 +0700

    minor changes

--- src/structures/Crypter.cpp
+++ src/structures/Crypter.cpp
@@ -1,12 +1,12 @@
-#include "../includes/Cryptographer.hpp"
+#include "../includes/Crypter.hpp"
 
-str_t& Cryptographer::get_key_for_encryption(){
+str_t& Crypter::get_key_for_encryption(){
     return key_for_encryption;
 }
 
-void Cryptographer::data_encryption(str_t src_name){
+void Crypter::data_encryption(str_t src_name, str_t data){
     // here we have to encrypt the data from the user or write the pre-encrypted data to a file
 }
-str_t& Cryptographer::data_decryption(str_t src_name){
+str_t& Crypter::data_decryption(str_t src_name){
     // here we have to decrypt the data from the file system that the user asked for
 }
